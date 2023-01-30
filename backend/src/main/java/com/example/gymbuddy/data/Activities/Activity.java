216eccc15bf309f8cbd79472521bf4e799a2815c
angela <angelalopez074@gmail.com>
2023-01-30 22:24:00 +0100

    backend : insert mock events

---
+++ backend/src/main/java/com/example/gymbuddy/data/Activities/Activity.java
@@ -0,0 +1,21 @@
+package com.example.gymbuddy.data.Activities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity
+@Getter
+@Table(schema="application", name="activities")
+public class Activity {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "code")
+    private String code;
+    @Basic
+    @Column(name = "label")
+    private String label;
+    @Basic
+    @Column(name = "description")
+    private String description;
+
+}
---
+++ backend/src/main/java/com/example/gymbuddy/data/Events/Event.java
@@ -0,0 +1,40 @@
+package com.example.gymbuddy.data.Events;
+
+import com.example.gymbuddy.data.Activities.Activity;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Table(schema="application", name="events")
+public class Event {
+
+    @Id
+    @Column(name = "id")
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name="activity_id", nullable=false)
+    private Activity activity;
+    @Basic
+    @Column(name = "date")
+    private LocalDateTime date;
+    @Basic
+    @Column(name = "organizer_id")
+    private int organizerId;
+    @Basic
+    @Column(name = "title")
+    private String title;
+    @Basic
+    @Column(name = "description")
+    private String description;
+    @Basic
+    @Column(name = "location")
+    private String location;
+
+}
